22e6c5aec3dbd2a453e3d2b8a3a813b115780cc5
codersurya0904 <89637956+surya0904shankar@users.noreply.github.com>
2022-01-06 09:30:28 +0530

    Update GAME_CODE.py

--- GAME_CODE.py
+++ GAME_CODE.py
@@ -7,45 +7,57 @@ while score_user!=5 and score_comp!=5:
     g=s.choice(k)
     w=input("enter choice: stone,paper or scissor").lower() 
     if g==w:
+        print("computer's choice",g)
         print("no points both are same")
+        print() 
         
     elif w=="stone" and g=="scissor":
        score_user+=1
+       print("computer's choice",g)
        print("you win a point")
        print("your score:",score_user,"computer's score:",score_comp)
+       print()
        
     elif w=="scissor" and g=="paper" :
        score_user+=1
+       print("computer's choice",g)
        print("you win a point")
        print("your score:",score_user,"computer's score:",score_comp)
+       print()
        
     elif w=="paper" and g=="stone":
        score_user+=1
+       print("computer's choice",g)
        print("you win a point")
-       print("your score:",score_user,"computer's score:",score_comp) 
+       print("your score:",score_user,"computer's score:",score_comp)
+       print() 
 
     elif g=="stone" and w=="scissor":
        score_comp+=1
+       print("computer's choice",g)
        print("computer wins a point")
        print("your score:",score_user,"computer's score:",score_comp) 
-        
+       print() 
 
     elif g=="scissor" and w=="paper" :
         score_comp+=1
+        print("computer's choice",g) 
         print("computer wins a point")
         print("your score:",score_user,"computer's score:",score_comp) 
-
+        print()
     elif g=="paper" and w=="stone":
         score_comp+=1
+        print("computer's choice",g)
         print("computer wins a point")
         print("your score:",score_user,"computer's score:",score_comp)
+        print()
+        
     else:
         print("invalid choice")
 if score_user > score_comp:
     print("great you won the match")
 elif  score_user < score_comp:
     print("sorry you lost  the match")
-
 
 
 
